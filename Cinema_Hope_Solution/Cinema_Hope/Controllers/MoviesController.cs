f09d5208ca1a4b666656243b4f8e88df78b14387
Waleed Bensumaidea <waleed.com145@gmail.com>
2023-11-13 00:37:39 +0300

    Add Movie Controller , Add Bootstrap Icons ,  Update _Layout

---
+++ Cinema_Hope_Solution/Cinema_Hope/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema_Hope.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
