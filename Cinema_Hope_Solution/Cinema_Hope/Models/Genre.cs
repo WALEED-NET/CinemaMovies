5c3bee4903b4fbdd536e8719f30d2975b2d01223
Waleed Bensumaidea <waleed.com145@gmail.com>
2023-11-13 01:35:24 +0300

    Create Genre.cs

---
+++ Cinema_Hope_Solution/Cinema_Hope/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿namespace Cinema_Hope.Models
+{
+    public class Genre
+    {
+        public int GenreId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public  string Name { get; set; }
+
+        public ICollection<Movie> Movies { get; set; }
+
+    }
+}
